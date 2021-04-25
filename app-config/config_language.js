85236a4d91a5dc2874fc384dfacc6b817bccc643
Christoph <email@ckloth.de>
2021-04-25 13:31:04 +0200

    divide language tool in to default and app specific

---
+++ app-config/config_language.js
@@ -0,0 +1,27 @@
+//--------------------------------------
+// jc://app-framework/, (c) Christoph Kloth
+//--------------------------------------
+// multi-language support (implementation just started)
+//--------------------------------------
+// language_app[LANG][<param>]
+// add your app specific translations
+
+var language_app = {
+	"DE" : {
+		"APP_TRANSLATION"	: "App spezifische Übersetzungen",
+		"ARCHIVE"		: "Archiv",
+		"CAMERAS"		: "Kameras",
+		"FAVORITS"             : "Favoriten",
+		"TODAY_COMPLETE"	: "Alle von heute",
+		"VIDEOS"               : "Videos",
+		},
+	"EN" : {
+		"APP_TRANSLATION"	: "App specific Translations",
+		"ARCHIVE"		: "Archive",
+		"CAMERAS"		: "Cameras",
+		"TODAY_COMPLETE"	: "Today complete",
+		"FAVORITS"             : "Favorits",
+		"VIDEOS"               : "Videos",
+		}
+	}
+
---
+++ app-fw/app-language.js
@@ -0,0 +1,93 @@
+//--------------------------------------
+// jc://music-box/, (c) Christoph Kloth
+//--------------------------------------
+// multi-language support (implementation just started)
+//--------------------------------------
+// language[LANG][<param>]
+// lang(<param>);
+
+var LANG     = "EN";
+var language = {
+	"DE" : {
+		"ADD"			: "Hinzuf&uuml;gen",
+		"CATEGORY"		: "Kategorie",
+		"CLOSE"		: "Schlie&szlig;en",
+		"DESCRIPTION"		: "Beschreibung",
+		"DEVICE"		: "Gerät",
+		"DEVICE_UNKNOWN"	: "Gerät unbekannt",
+		"DISC_SPACE"		: "Speicherplatz",
+		"EDIT"			: "Bearbeiten",
+		"EMPTY_LIST"		: "Liste ist leer",
+		"ERROR"		: "Fehler",
+		"INFORMATION"		: "Informationen",
+		"LIVESTREAM"		: "Live-Stream",
+		"LOADING_APP"		: "Starte App",
+		"LOADING_TIME"		: "Ladezeiten",
+		"LOADING_DATA"		: "Lade Daten ...",
+		"LOGO"			: "Logo",
+		"NEW"			: "neu",
+		"NOT_FOUND"		: "nicht gefunden",
+		"PLEASE_WAIT"		: "Bitte warten",
+		"RELOAD"		: "neu laden",
+		"RELOAD_DATA"		: "Daten neu laden",
+		"RELOAD_STARTED"	: "Reload gestartet",
+		"SETTINGS"		: "Einstellungen",
+		"SIZE"			: "Gr&ouml;&szlig;e",
+		"STARTTIME"		: "Startzeit",
+		"TITLE"		: "Titel",
+		"TODAY"		: "Heute",
+		"PLEASE_WAIT"		: "Bitte warten",
+		"YESTERDAY"		: "Gestern",
+		"WEBSITE"		: "Webseite",
+		},
+	"EN" : {
+		"ADD"			: "Add",
+		"CATEGORY"		: "Category",
+		"CLOSE"		: "Close",
+		"DESCRIPTION"		: "Description",
+		"DEVICE"		: "Device",
+		"DEVICE_UNKNOWN"	: "Device unknown",
+		"DISC_SPACE"		: "Disc space",
+		"EDIT"			: "Edit",
+		"EMPTY_LIST"		: "List is empty",
+		"ERROR"		: "Error",
+		"HERE"			: "here",
+		"INFORMATION"		: "Information",
+		"LIVESTREAM"		: "Live stream",
+		"LOADING_APP"		: "Loading App",
+		"LOADING_TIME"		: "Loading Time",
+		"LOADING_DATA"		: "Loading data ...",
+		"LOGO"			: "Logo",
+		"NEW"			: "new",
+		"NOT_FOUND"		: "not found",
+		"PLEASE_WAIT"		: "Please wait",
+		"RELOAD"		: "reload",
+		"RELOAD_DATA"		: "Reload data",
+		"RELOAD_STARTED"	: "Reload started",
+		"SETTINGS"		: "Settings",
+		"SIZE"			: "Size",
+		"STARTTIME"		: "Start time",
+		"STREAM"		: "Stream",
+		"STREAMS"		: "Streams",
+		"TITLE"		: "Title",
+		"TODAY"		: "Today",
+		"PLEASE_WAIT"		: "Please wait",
+		"YESTERDAY"		: "Yesterday",
+		"WEBSITE"		: "Website",
+		}
+	}
+
+
+function lang( param ) {
+        var error = "<font color='red'>Translation not found</font>";
+	if (language[LANG][param])		{ return language[LANG][param]; }
+        else if (language["EN"][param])	{ return language["EN"][param]; }
+        else if (language["DE"][param])	{ return language["DE"][param]; }
+        else if (language_app != undefined) {
+		if (language_app[LANG][param])	{ return language_app[LANG][param]; }
+	        else if (language_app["EN"][param])	{ return language_app["EN"][param]; }
+	        else if (language_app["DE"][param])	{ return language_app["DE"][param]; }
+		else					{ return error; }
+        	}
+	else { return error; }
+	}

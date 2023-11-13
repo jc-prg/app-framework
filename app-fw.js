6397ae1cbdad2942a8130a27bbc7d0fe8fa6a6ac
Christoph <email@ckloth.de>
2023-11-13 21:57:30 +0100

    app: load js and css files dynamically

---
+++ app-fw.js
@@ -0,0 +1,35 @@
+//--------------------------------
+// jc://app-framework/
+//--------------------------------
+
+app_js_init = [
+    "app-fw/app-functions.js",
+    "app-fw/app-html-elements.js",
+    "app-fw/app-menu.js",
+    "app-fw/app-language.js",
+    ];
+
+app_js_config = [
+    "app-fw/config_stage.sample.js",
+    "app-fw/config_language.sample.js",
+    "app-fw/config_main.sample.js",
+    ];
+
+app_js_start = [
+    "app-fw/app-conf.js",
+    "app-fw/app-main.js",
+    ];
+    
+app_css = [
+    "app-fw/style-main.css",
+    "app-fw/style-menu.css",
+    "app-fw/style-frame.css",
+    "app-fw/style-status.css",
+    "app-fw/style-main-iphone.css",
+    "app-fw/style-main-laptop.css",
+    "app-fw/style-frame-iphone.css",
+    "app-fw/style-frame-laptop.css",
+    "app-fw/style-main-dark.css",
+    "app-fw/style-frame-dark.css",
+    "app-fw/style-menu-dark.css"
+    ];
